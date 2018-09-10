770d76e90941983c3cb7759c43788615ed8c93d4
Troy Tae <tjy970721@gmail.com>
2018-09-10 14:12:26 +0900

    absoluteTop/Left

--- src/element-position.js
+++ src/element-position.js
@@ -1,4 +1,41 @@
 (function () {
+    var Position = {
+        getOffset: function(dom) {
+            var left = dom.offsetLeft,
+                top = dom.offsetTop
+            ;
+            while (dom = dom.offsetParent) {
+                left += dom.offsetLeft;
+                top += dom.offsetTop;
+            }
+            return {
+                left: left,
+                top: top
+            };
+        },
+        getScroll: function(dom) {
+            var left = 0,
+                top = 0
+            ;
+            while (dom = dom.parentElement) {
+                left += dom.scrollLeft;
+                top += dom.scrollTop;
+            }
+            return {
+                left: left,
+                top: top
+            };
+        },
+        getAbsolute: function(dom) {
+            var offsetPos = Position.getOffset(dom),
+                scrollPos = Position.getScroll(dom)
+            ;
+            return {
+                left: (offsetPos.left - scrollPos.left),
+                top: (offsetPos.top - scrollPos.top)
+            };
+        }
+    };
     Object.defineProperty(HTMLElement.prototype, 'offsetRight', {
         get: function () {
             return this.offsetLeft + this.offsetWidth;
@@ -7,6 +44,18 @@
     Object.defineProperty(HTMLElement.prototype, 'offsetBottom', {
         get: function () {
             return this.offsetTop + this.offsetHeight;
+        }
+    });
+    Object.defineProperty(HTMLElement.prototype, 'absoluteTop', {
+        get: function () {
+            var absolutePos = Position.getAbsolute(this);
+            return absolutePos.top;
+        }
+    });
+    Object.defineProperty(HTMLElement.prototype, 'absoluteLeft', {
+        get: function () {
+            var absolutePos = Position.getAbsolute(this);
+            return absolutePos.left;
         }
     });
 }());
