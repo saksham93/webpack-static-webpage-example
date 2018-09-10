331fca5827a3c63d6531ee53d359b15addba5563
Troy Tae <tjy970721@gmail.com>
2018-09-10 13:52:50 +0900

    offsetRight/Bottom

--- src/element-position.js
+++ src/element-position.js
@@ -1,3 +1,12 @@
 (function () {
-    console.log('test');
+    Object.defineProperty(HTMLElement.prototype, 'offsetRight', {
+        get: function () {
+            return this.offsetLeft + this.offsetWidth;
+        }
+    });
+    Object.defineProperty(HTMLElement.prototype, 'offsetBottom', {
+        get: function () {
+            return this.offsetTop + this.offsetHeight;
+        }
+    });
 }());
