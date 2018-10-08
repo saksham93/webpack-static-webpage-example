8024f851de20c8f95f17752aafe0144bb059bfec
Troy Tae <tjy970721@gmail.com>
2018-10-08 18:55:23 +0900

    apply uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
+import pkg from './package.json';
+import {uglify} from 'rollup-plugin-uglify';
 import typescript from 'rollup-plugin-typescript';
+
 
 export default {
     input: 'src/main.ts',
     output: {
-        file: 'dist/element-position.js',
+        file: pkg.main,
         format: 'umd',
         name: 'Position'
     },
     plugins: [
-        typescript()
+        typescript(),
+        uglify()
     ]
 };
